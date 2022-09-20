92fda6c67dd3cbe83058c22f28404a6caf100bdd
Adam Leskis <leskis@gmail.com>
2022-09-20 23:05:48 +0100

    add an env var for node

--- randomize_suspects.sh
+++ randomize_suspects.sh
@@ -1,7 +1,7 @@
 # we want to randomize which process is actually the "noisy" one, so this makes sure each run won't just be the same (boring!)
 shuf .env.suspects > .env.suspects.tmp && mv .env.suspects.tmp .env.suspects
 count=1
-for LANGUAGE in NODE PYTHON GOLANG; do
+for LANGUAGE in NODE PYTHON GOLANG RUST; do
   sed -i "${count} s/./${LANGUAGE}_LOG=&/" .env.suspects
   (( count++ ))
 done < .env.suspects

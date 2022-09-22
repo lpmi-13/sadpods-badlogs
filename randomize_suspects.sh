05d3a42216fa50aacaa55d5fe93d18ce8f5bc278
Adam Leskis <leskis@gmail.com>
2022-09-23 00:11:22 +0100

    simplify randomize script

--- randomize_suspects.sh
+++ randomize_suspects.sh
@@ -1,7 +1,5 @@
 # we want to randomize which process is actually the "noisy" one, so this makes sure each run won't just be the same (boring!)
 shuf .env.suspects > .env.suspects.tmp && mv .env.suspects.tmp .env.suspects
-count=1
 for number in 1 2 3 4 5; do
-  sed -i "${count} s/./LOG_LEVEL=&/" .env.suspects
-  (( count++ ))
+  sed -i "${number} s/./LOG_LEVEL=&/" .env.suspects
 done < .env.suspects

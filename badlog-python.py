9b02cb7f4e95529905ac63047fc18f0469b43a2d
Adam Leskis <leskis@gmail.com>
2022-09-23 00:01:40 +0100

    send env vars to processes

--- badlog-python.py
+++ badlog-python.py
@@ -6,10 +6,10 @@ import random
 import time
 from datetime import datetime
 
-VERBOSE_LOGS = os.getenv('PYTHON_LOG', '0')
+VERBOSE_LOGS = os.getenv('LOG_LEVEL', '0')
 
 if VERBOSE_LOGS == '1':
-    f = open('/var/log/badpython.log', 'a')
+    f = open('/var/log/bad.log', 'a')
     while True:
         r = random.randrange(1111111111, 9999999999)
         f.write(str(datetime.now()) + ' token: ' + str(r) + '\n')
